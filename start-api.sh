fa70f481e3cd24219f242c54d65498289b9781f3
GarondEisenfaust <chr.schr@web.de>
2022-05-26 16:16:19 +0200

    unlock with a password file

--- start-api.sh
+++ start-api.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 record=$(./get-node-record.sh)
-geth --datadir api --networkid 57771 --port 30305 --bootnodes $record --http.corsdomain '*' --http --http.api personal,eth,net,web3 
+geth --datadir api --networkid 57771 --port 30305 --bootnodes $record --allow-insecure-unlock --unlock 0x250548444A4fBcFfb273B1034aa32cD6828544b0 --password super-secret-password --http.corsdomain '*' --http --http.api personal,eth,net,web3 
--- start-signer.sh
+++ start-signer.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 record=$(./get-node-record.sh)
-geth --datadir signer --networkid 57771 --port 30304 --bootnodes $record --unlock 0x14ef5f7ad517e2be0f1c3159e83cc393c588e541 --mine 
+geth --datadir signer --networkid 57771 --port 30304 --bootnodes $record --unlock 0x14ef5f7ad517e2be0f1c3159e83cc393c588e541 --mine --password super-secret-password
